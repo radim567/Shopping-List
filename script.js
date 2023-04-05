489dd7fb1325ecb88b691ee256c0ae77d7d66bb9
Radim Vrška <87022843+radim567@users.noreply.github.com>
2023-04-05 19:27:45 +0200

    Another day...

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
-const itemList = document.getElementById("item-ls");
+const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
 
-function addItem(e) {
+function onAddItemSubmit(e) {
   e.preventDefault();
+
   const newItem = itemInput.value;
+
   // Validate input
-  if (newItem === "") {
+  if (newItem.trim() === "") {
     alert("Please add an item");
     return;
   }
 
+  addItemToDOM(newItem);
+
+  checkUI();
+
+  itemInput.value = "";
+}
+
+function addItemToDOM(item) {
   // Create list item
   const li = document.createElement("li");
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(item));
 
   const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
 
+  // Add li to the DOM
   itemList.appendChild(li);
-
-  itemInput.value = " ";
-
-  console.log(itemList);
 }
 
 function createButton(classes) {
@@ -30,8 +39,6 @@ function createButton(classes) {
   button.className = classes;
   const icon = createIcon("fa-solid fa-xmark");
   button.appendChild(icon);
-  button.appendChild(document.createTextNode("X"));
-
   return button;
 }
 
@@ -41,5 +48,52 @@ function createIcon(classes) {
   return icon;
 }
 
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are you sure?")) {
+    }
+    e.target.parentElement.parentElement.remove();
+    checkUI();
+  }
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+}
+
+function filterItems(e) {
+  const items = itemList.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
+
 // Event Listeners
-itemForm.addEventListener("submit", addItem);
+
+itemForm.addEventListener("submit", onAddItemSubmit);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", filterItems);
+
+checkUI();
